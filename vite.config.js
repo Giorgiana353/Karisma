395f4582f3dcd4f5cd4c3521e8296bfc67f7c097
Giorgiana <giorgiana.dinu@emanuel.ro>
2024-11-15 12:35:07 +0200

    test2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ export default defineConfig({
    base: '',
    build:{
     rollupOptions: {
-      input: [ "./index.html", "./epilare.html", "./coafor.html", "./contact.html", "./extensii.html", "./laminare.html", "./makeup.html", "./noi.html", "./oferte.html", "./parere.html", "./servicii.html", "./preturi.html"]
+      input: [ "./index.html", "./epilare.html", "./coafor.html", "./contact.html", "./extensii.html", "./laminare.html", "./makeup.html", "./noi.html", "./oferte.html", "./parere.html", "./servicii.html", "./preturi.html", "./tpar.html", "./tface.html"]
     }
   }
 })
