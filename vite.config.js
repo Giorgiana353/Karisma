96596aba07cf296d2684e3fabc465f0f378f2a57
Giorgiana <giorgiana.dinu@emanuel.ro>
2024-11-28 15:30:55 +0200

    "gata"

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ export default defineConfig({
    base: '',
    build:{
     rollupOptions: {
-      input: [ "./index.html", "./epilare.html", "./coafor.html", "./contact.html", "./extensii.html", "./laminare.html", "./makeup.html", "./noi.html", "./oferte.html", "./parere.html", "./servicii.html", "./preturi.html", "./tpar.html", "./tface.html"]
+      input: [ "./index.html", "./epilare.html", "./coafor.html", "./contact.html", "./extensii.html", "./laminare.html", "./makeup.html", "./noi.html", "./oferte.html", "./parere.html", "./servicii.html", "./preturi.html", "./tpar.html", "./tface.html", "./cosmetica.html", "./locatie.html", "./masaj.html", "./tatuaj.html", "./cursuri.html"]
     }
   }
 })
